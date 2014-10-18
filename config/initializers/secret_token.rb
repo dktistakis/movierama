0ec16f68abc6ab0459f82568451ccb6f0204e324
Dimitris <dimitrisktistakis.dk@gmail.com>
2014-10-18 10:56:27 +0300

    adds first files and checks connection

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movierama::Application.config.secret_token = 'a2df04ffe0266a43228face65c13368f5c6475319831ab70c4d552df3d1fb31a2c90e26a8c9e30e9ac8c0d3792b9148e1d83a6f36fa94212f69feb59bc7ee011'
