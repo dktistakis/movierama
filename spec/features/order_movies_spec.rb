346d7ee87813fcf6c04f44b70a48b7cb8115d2f3
Dimitris <dimitrisktistakis.dk@gmail.com>
2014-10-21 10:29:03 +0300

    fixes problem with console testing for sorting

--- spec/features/order_movies_spec.rb
+++ spec/features/order_movies_spec.rb
@@ -3,15 +3,25 @@ require 'spec_helper'
 feature "Order movies" do
   let(:u) { FactoryGirl.create(:user) }
   let(:u1) { FactoryGirl.create(:user, name:"othername", email:"email@gmail.com") }
+  let(:u2) { FactoryGirl.create(:user, name:"othername1", email:"email1@gmail.com") }
 
-  let(:m1) { FactoryGirl.create(:movie, user_id: u.id, likes_count: 1, hates_count: 4)}
-  let(:m2) { FactoryGirl.create(:movie, user_id: u1.id, title:"title1", likes_count: 5, hates_count: 6)}
-  let(:m3) { FactoryGirl.create(:movie, user_id: u1.id, title:"title2", likes_count: 3, hates_count: 3)}
+  let(:m1) { FactoryGirl.create(:movie, user_id: u.id)}
+  let(:m2) { FactoryGirl.create(:movie, user_id: u1.id, title:"title1")}
+  let(:m3) { FactoryGirl.create(:movie, user_id: u1.id, title:"title2")}
+
+  let(:l1) { FactoryGirl.create(:like, user_id: u.id, movie_id: m2.id )}
+  let(:l2) { FactoryGirl.create(:like, user_id: u.id, movie_id: m3.id )}
+  let(:l3) { FactoryGirl.create(:like, user_id: u2.id, movie_id: m2.id )}
+
+  let(:h1) { FactoryGirl.create(:hate, user_id: u1.id, movie_id: m1.id )}
+  let(:h2) { FactoryGirl.create(:hate, user_id: u2.id, movie_id: m1.id )}
+  let(:h3) { FactoryGirl.create(:hate, user_id: u.id, movie_id: m3.id )}
 
   scenario 'by date' do
     m1.reload
     m2.reload
     m3.reload
+
     visit root_path
 
     m3.title.should appear_before(m2.title)
@@ -22,6 +32,10 @@ feature "Order movies" do
     m1.reload
     m2.reload
     m3.reload
+    l1.reload
+    l2.reload
+    l3.reload
+
     visit root_path
 
     click_link 'Likes'
@@ -30,15 +44,19 @@ feature "Order movies" do
     m3.title.should appear_before(m1.title)
   end
 
-  scenario 'by likes' do
+  scenario 'by hates' do
     m1.reload
     m2.reload
     m3.reload
+    h1.reload
+    h2.reload
+    h3.reload
+
     visit root_path
 
     click_link 'Hates'
 
-    m2.title.should appear_before(m1.title)
     m1.title.should appear_before(m3.title)
+    m3.title.should appear_before(m2.title)
   end
 end
